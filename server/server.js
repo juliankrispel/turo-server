809368774a87b91f98a7358e6e4a367019f6f066
Julian Krispel-Samsel <julian@goodafternoon.co>
2015-02-19 11:44:23 +0100

    change ejs module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var ejs = require('express-ejs-layouts');
+var ejs = require('ejs');
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 var repository = require('./repository');
 var routes = require('./routes');
@@ -8,13 +9,13 @@ var multer = require('multer');
 
 module.exports = function(port){
     // view engine
-
-    app.set('views', __dirname + '/../node_modules/turo-desktop/public/templates');
-    app.set('view engine', 'ejs');
-    app.use(ejs);
+    app.engine('.html', ejs.__express);
+    app.set('views', path.join(__dirname, '../node_modules/turo-desktop/public/templates'));
+    app.set('view engine', 'html');
+    console.log(app.get('view engine'));
 
     // for static files
-    app.use(express.static(__dirname + '/../node_modules/turo-desktop/public'));
+    //app.use(express.static(path.join(__dirname, '/../node_modules/turo-desktop/public')));
 
     // for parsing application/json
     app.use(bodyParser.json()); 
